2aa88230eb4afe65c9576c12bd8675b884e41b43
akilhans <akilhanmedia@gmail.com>
2025-03-31 23:30:10 +0500

    replaced the logo

--- app/rahmat/page.tsx
+++ app/rahmat/page.tsx
@@ -7,12 +7,13 @@ export default function SuccessPage() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-50">
             <div className="w-full max-w-md bg-white rounded-lg shadow-sm p-8 text-center">
-                <div className="flex justify-center mb-6">
-                    <CheckCircle2 className="h-24 w-24 text-green-500" />
-                </div>
                 <div className="flex justify-center mb-4">
                     <Image src="/logo/logo.png" width={150} height={50} alt="Logo" />
                 </div>
+                <div className="flex justify-center mb-6">
+                    <CheckCircle2 className="h-24 w-24 text-green-500" />
+                </div>
+
                 <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">ARIZA TOPSHIRILDI</h1>
                 <p className="text-xl text-green-500 font-medium mb-8">Sizning arizangiz qabul qilindi!</p>
                 <Link href="/">
